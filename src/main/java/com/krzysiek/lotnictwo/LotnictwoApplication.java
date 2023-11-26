8cff94b606152cf48da88a74863355ba256d55d2
Administrator <admin@example.com>
2023-11-26 16:09:06 +0100

    Project structure

---
+++ src/main/java/com/krzysiek/lotnictwo/LotnictwoApplication.java
@@ -0,0 +1,13 @@
+package com.krzysiek.lotnictwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotnictwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotnictwoApplication.class, args);
+	}
+
+}
